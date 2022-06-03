a1697d3afe08726f7ee98ba08b58aa7d10004949
MGurcan <mehmetgurcaneducation@gmail.com>
2022-06-03 15:31:59 +0300

    description bar added, style changed

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ dotenv.config();
 
 app.use("/static", express.static("public"));
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 //connection to db
 //mongoose.set("useFindAndModify", false);
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
-console.log("Connected to db!");
-app.listen(3000, () => console.log("Server Up and running"));
+    console.log("Connected to db!");
+    app.listen(3000, () => console.log("Server Up and running"));
 });
 
 
@@ -23,45 +23,54 @@ app.listen(3000, () => console.log("Server Up and running"));
 
 
 
-    app.set("view engine", "ejs");
+app.set("view engine", "ejs");
 
 
 
 
-    app.get("/", (req, res) => {
-        TodoTask.find({}, (err, tasks) => {
+app.get("/", (req, res) => {
+    TodoTask.find({}, (err, tasks) => {
         res.render("todo.ejs", { todoTasks: tasks });
-        });
-        });
+    });
+});
 
-    app.post('/',async (req, res) => {
-        const todoTask = new TodoTask({
-        content: req.body.content
-        });
-        try {
+app.post('/', async (req, res) => {
+
+
+    console.log("add button pressed!");
+    console.log(req.body);
+    console.log(req.body.content);
+
+    console.log("description:  " + req.body.description);
+    const todoTask = new TodoTask({
+        content: req.body.content,
+        description: req.body.description
+    });
+
+    try {
         await todoTask.save();
         res.redirect("/");
-        } catch (err) {
+    } catch (err) {
         res.redirect("/");
-        }
-        });
+    }
+});
 
-        //UPDATE
+//UPDATE
 app
-.route("/edit/:id")
-.get((req, res) => {
-const id = req.params.id;
-TodoTask.find({}, (err, tasks) => {
-res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
-});
-})
-.post((req, res) => {
-const id = req.params.id;
-TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
-if (err) return res.send(500, err);
-res.redirect("/");
-});
-});
+    .route("/edit/:id")
+    .get((req, res) => {
+        const id = req.params.id;
+        TodoTask.find({}, (err, tasks) => {
+            res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+        });
+    })
+    .post((req, res) => {
+        const id = req.params.id;
+        TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+            if (err) return res.send(500, err);
+            res.redirect("/");
+        });
+    });
 
 
 //delete
@@ -69,8 +78,8 @@ res.redirect("/");
 app.route("/remove/:id").get((req, res) => {
     const id = req.params.id;
     TodoTask.findByIdAndRemove(id, err => {
-    if (err) return res.send(500, err);
-    res.redirect("/");
+        if (err) return res.send(500, err);
+        res.redirect("/");
     });
-    });
+});
 
